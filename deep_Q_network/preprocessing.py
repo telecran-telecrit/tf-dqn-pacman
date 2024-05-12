537229ec6b8a594782ecac0f466eac41e48bb6ee
tel Ecran tel Ecrit <telecran-telecrit@127.0.0.1>
2024-05-12 15:53:29 +0300

    preprocessing

---
+++ deep_Q_network/preprocessing.py
@@ -0,0 +1,41 @@
+from .parameters import PACMAN_COLOR_GRAY, WALL_COLOR_GRAY, BACKGROUND_GRAY
+from ale_py import ALEInterface
+from typing import List
+import numpy as np
+import cv2
+
+from tensorflow.keras.preprocessing.image import img_to_array
+
+#from collections import Counter
+#from time import perf_counter as pf
+
+#WALL_COLOR_GRAY = cv2.cvtColor(np.array([[WALL_COLOR]], dtype=np.uint8), cv2.COLOR_RGB2GRAY)[0][0]
+#BACKGROUND_GRAY = cv2.cvtColor(np.array([[BACKGROUND]], dtype=np.uint8), cv2.COLOR_RGB2GRAY)[0][0]
+#PACMAN_COLOR_GRAY = cv2.cvtColor(np.array([[PACMAN_COLOR]], dtype=np.uint8), cv2.COLOR_RGB2GRAY)[0][0]
+
+def extend_walls(img: np.array):
+    extension = np.array([[WALL_COLOR_GRAY for x in range(160)] for y in range(3)])
+    return np.concatenate([extension, img, extension])
+
+
+def unit_prepr_obs(obs: np.array):
+    gray_img = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
+    trimmed_img = gray_img[1:171]
+    extended_img = extend_walls(trimmed_img)
+    final_img = extended_img[1::4, 1::4]
+    return np.stack([final_img.astype(np.float32)])
+
+
+def preprocess_observation(observations: List[np.array], new_obs: np.array):
+    for i in range(3):
+        observations[3 - i] = observations[2 - i]
+    observations[0] = unit_prepr_obs(new_obs)
+    state = np.concatenate(observations) # state = np.concatenate(observations);  screen = torch.from_numpy(state)
+    return img_to_array(state) # screen.unsqueeze(0)
+
+
+def init_obs(env: ALEInterface):
+    return [unit_prepr_obs(env.step(0)[0]) for i_step in range(4)][::-1]
+
+    
+    
